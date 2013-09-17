bf1b2432fe377b600fdb649640560aa55127224f
Connor4898 <connor.b1998@yahoo.co.uk>
2013-09-17 18:28:06 +0100

    Added Custom Creative Inventory

---
+++ Custom-Creative-Inventory.js
@@ -0,0 +1,21 @@
+/*
+Custom Creative Inventory
+Made by Connor4898
+
+    © Copyright 2013 Connor4898
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
+function procCmd(c) {
+    var p = c.split(" ");
+    var command = p[0];
+    switch(command) {
+        case 'setItem': {
+            if(p[1] > 0 && p[1] < 457) {
+                setCarriedItem(getPlayerEnt(),p[1]);
+            }
+        }
+    }
+}
